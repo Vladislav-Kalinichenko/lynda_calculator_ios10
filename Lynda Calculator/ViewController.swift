b6f5f6135024008c21f890bd6ee0f3d8b154d840
Vladislav Kalinichenko <kalinichenko_vlad@ukr.net>
2017-03-10 03:00:25 +0200

    functionality implementation for addition and subtraction

--- Lynda Calculator/ViewController.swift
+++ Lynda Calculator/ViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum modes {
+    case not_set
+    case addition
+    case subtration
+}
 
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString:String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: .addition)
+    }
+    @IBAction func didPressSubtract(_ sender: Any) {
+        changeMode(newMode: .subtration)
+    }
+    @IBAction func didPressEquals(_ sender: Any) {
+        guard let labelInt:Int = Int(labelString) else {
+            return
+        }
+        if (currentMode == .not_set || lastButtonWasMode){
+            return
+        }
+        if (currentMode == .addition){
+            savedNum += labelInt
+        } else if (currentMode == .subtration) {
+            savedNum -= labelInt
+        }
+        currentMode = .not_set
+        labelString = "\(savedNum)"
+        updateText()
+        lastButtonWasMode = true
+    }
+    @IBAction func didPressClear(_ sender: Any) {
+        labelString = "0"
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        label.text = "0"
+    }
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        let stringValue:String? = sender.titleLabel?.text
+        
+        if (lastButtonWasMode){
+            lastButtonWasMode = false
+            labelString = "0"
+        }
+        
+        labelString = labelString.appending(stringValue!)
+        updateText()
+    }
+    
+    func updateText(){
+        guard let labelInt:Int = Int(labelString) else {
+            return
+        }
+        if (currentMode == .not_set){
+            savedNum = labelInt
+        }
+        
+        label.text = "\(labelInt)"
+    }
+    
+    func changeMode(newMode: modes){
+        if (savedNum == 0){
+            return
+        }
+        currentMode = newMode
+        lastButtonWasMode = true
+    }
+    
 }
 
